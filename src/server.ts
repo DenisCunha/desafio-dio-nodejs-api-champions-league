437903d8c85601d0e6b6e0362e8ff7dbe727fa25
deniscunha <ded_2004@hotmail.com>
2024-07-01 14:07:11 -0300

    config api with express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("server init");
+import express, {Request, Response} from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.get("/", (request: Request, response: Response) => {
+
+console.log("Server Init");
+
+});
+
+app.listen(3000);
